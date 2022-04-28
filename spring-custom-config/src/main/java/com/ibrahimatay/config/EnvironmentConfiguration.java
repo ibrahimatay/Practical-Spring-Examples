ca37c23bcfd62d61dd679902dfed82cff80cbcea
İbrahim ATAY <atay363@gmail.com>
2022-04-28 12:35:49 +0300

    spring-custom-config

---
+++ spring-custom-config/src/main/java/com/ibrahimatay/config/EnvironmentConfiguration.java
@@ -0,0 +1,13 @@
+package com.ibrahimatay.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EnvironmentConfiguration {
+    private String name;
+    private String url;
+}
