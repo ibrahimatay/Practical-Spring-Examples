31882abfe7c6bd823e223f8e6759a53e702619eb
İbrahim ATAY <atay363@gmail.com>
2022-05-15 19:52:32 +0300

    servlet serve problem solved

--- servlet-config-with-annotations/src/main/java/com/ibrahimatay/MyServlet.java
+++ servlet-config-with-annotations/src/main/java/com/ibrahimatay/MyServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+@WebServlet(name="ServletConfigClass",urlPatterns={"/hello"})
 public class MyServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
