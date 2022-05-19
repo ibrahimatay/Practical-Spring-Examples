1c56fee8e1af8e83a686827659ad2fc68ce55576
İbrahim ATAY <atay363@gmail.com>
2022-05-19 20:42:55 +0300

    servlet-filters-with-annotations

---
+++ servlet-filters-with-annotations/src/main/java/com/ibrahimatay/MyFilter.java
@@ -0,0 +1,31 @@
+package com.ibrahimatay;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebFilter
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("MyFilter#init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("MyFilter#doFilter - invoked before");
+        System.out.println("MyFilter#doFilter - invoked before");
+
+        chain.doFilter(request, response);
+
+        printWriter.println("MyFilter#doFilter - invoked after");
+        System.out.println("MyFilter#doFilter - invoked after");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("MyFilter#destroy");
+    }
+}
